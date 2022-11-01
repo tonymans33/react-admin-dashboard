d1c5f91d06fa97f73e4aaf726999cdefb109c2e2
tonymans33 <tonymansour66@yahoo.com>
2022-11-02 00:54:44 +0200

    implementing colorPicker, calendar, kanban and editor apps

--- src/pages/Calendar.jsx
+++ src/pages/Calendar.jsx
@@ -1,8 +1,17 @@
+import { Agenda, Day, DragAndDrop, Inject, Month, Resize, ScheduleComponent, Week, WorkWeek } from '@syncfusion/ej2-react-schedule'
 import React from 'react'
+import { Header } from '../components'
+import { scheduleData } from '../data/dummy'
 
 function Calendar() {
   return (
-    <div>Calendar</div>
+    <div className='m-2 md:m-10 mt-24 p-2 md:p-10 bg-white rounded-3xl'>
+      <Header category='App' title='Calendar' />
+      <ScheduleComponent height='650px' eventSettings={{ dataSource: scheduleData }} selectedDate={new Date(2021, 0, 10)}>
+        <Inject  services={[Day, Week, WorkWeek, Month, Agenda, Resize, DragAndDrop]} />
+      </ScheduleComponent>
+
+    </div>
   )
 }
 
